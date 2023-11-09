376011771caa111b011f348ade638ddb6eeaea2c
camilamarendazzo <camimarendazzo@gmail.com>
2023-11-08 22:28:04 -0300

    Premium en LS

--- js/pagar.js
+++ js/pagar.js
@@ -30,77 +30,85 @@ d.addEventListener("DOMContentLoaded", (e) => {
       location.href = "../../index.html";
     }
   });
+
+  let URLActual = window.location.search;
+  let pathActual = window.location.pathname;
+  let URLPartida = URLActual.split("=");
+  const NOMBRE_PLAN = URLPartida[1].toUpperCase();
+
+  const TITULO = (document.querySelector(".plan-elegido").innerHTML =
+    "Ha elegido el PLAN " + NOMBRE_PLAN);
+
+  if (NOMBRE_PLAN == "MENSUAL") {
+    document.querySelector(".img").innerHTML =
+      '<img src="../../assets/img/PlanMensual.jpg">';
+    document.querySelector(".precio").innerHTML = "Total: $900";
+  } else if (NOMBRE_PLAN == "ANUAL") {
+    document.querySelector(".img").innerHTML =
+      '<img src="../../assets/img/PlanAnual.jpg">';
+    document.querySelector(".precio").innerHTML = "Total: $9000";
+  } else if (NOMBRE_PLAN == "INFINITO") {
+    document.querySelector(".img").innerHTML =
+      '<img src="../../assets/img/PlanInfinito.jpg">';
+    document.querySelector(".precio").innerHTML = "Total: $15000";
+  }
+
+  const BTN_PAGAR = document.querySelector(".btnPagar");
+  const DIALOG = document.querySelector(".modal");
+  const BTN_MODAL = document.querySelector(".btnModal");
+
+  BTN_MODAL.addEventListener("click", (e) => {
+    DIALOG.close();
+    document.querySelector(".cajota").submit();
+  });
+
+  BTN_PAGAR.addEventListener("click", (e) => {
+    let nroTarjeta = document.querySelector("#Tarjeta").value;
+    let cod = document.querySelector("#CVC").value;
+    let vto = document.querySelector("#VTO").value;
+    let nombre = document.querySelector("#NombreYApellido").value;
+    let hasError = false;
+    document.querySelector(".alertaTarjeta").innerHTML = "";
+    document.querySelector(".alertaCod").innerHTML = "";
+    document.querySelector(".alertaVTO").innerHTML = "";
+    document.querySelector(".alertaNombre").innerHTML = "";
+
+    if (nroTarjeta.length != 16) {
+      e.preventDefault();
+      document.querySelector(".alertaTarjeta").innerHTML =
+        "Número de tarjeta inválido";
+      hasError = true;
+    }
+    if (cod.length != 3 || cod == "000" || cod == "999") {
+      e.preventDefault();
+      document.querySelector(".alertaCod").innerHTML =
+        "Código de seguridad inválido";
+      hasError = true;
+    }
+    if (vto.length == 0) {
+      e.preventDefault();
+      document.querySelector(".alertaVTO").innerHTML =
+        "Fecha de vencimiento inválida";
+      hasError = true;
+    }
+    if (nombre.length == 0) {
+      e.preventDefault();
+      document.querySelector(".alertaNombre").innerHTML =
+        "Nombre y apellido inválidos";
+      hasError = true;
+    }
+
+    if (!hasError) {
+      e.preventDefault();
+      datosDesdeLS.usuarios.forEach((usuario) => {
+        if (usuario.estado){
+          usuario.premium = true;
+          localStorage.setItem("datos", JSON.stringify(datosDesdeLS));
+        }
+      })
+      DIALOG.showModal();
+    }
+  });
 });
 
-let URLActual = window.location.search;
-let pathActual = window.location.pathname;
-let URLPartida = URLActual.split("=");
-const NOMBRE_PLAN = URLPartida[1].toUpperCase();
 
-const TITULO = (document.querySelector(".plan-elegido").innerHTML =
-  "Ha elegido el PLAN " + NOMBRE_PLAN);
-
-if (NOMBRE_PLAN == "MENSUAL") {
-  document.querySelector(".img").innerHTML =
-    '<img src="../../assets/img/PlanMensual.jpg">';
-  document.querySelector(".precio").innerHTML = "Total: $900";
-} else if (NOMBRE_PLAN == "ANUAL") {
-  document.querySelector(".img").innerHTML =
-    '<img src="../../assets/img/PlanAnual.jpg">';
-  document.querySelector(".precio").innerHTML = "Total: $9000";
-} else if (NOMBRE_PLAN == "INFINITO") {
-  document.querySelector(".img").innerHTML =
-    '<img src="../../assets/img/PlanInfinito.jpg">';
-  document.querySelector(".precio").innerHTML = "Total: $15000";
-}
-
-const BTN_PAGAR = document.querySelector(".btnPagar");
-const DIALOG = document.querySelector(".modal");
-const BTN_MODAL = document.querySelector(".btnModal");
-
-BTN_MODAL.addEventListener("click", (e) => {
-  DIALOG.close();
-  document.querySelector(".cajota").submit();
-});
-
-BTN_PAGAR.addEventListener("click", (e) => {
-  let nroTarjeta = document.querySelector("#Tarjeta").value;
-  let cod = document.querySelector("#CVC").value;
-  let vto = document.querySelector("#VTO").value;
-  let nombre = document.querySelector("#NombreYApellido").value;
-  let hasError = false;
-  document.querySelector(".alertaTarjeta").innerHTML = "";
-  document.querySelector(".alertaCod").innerHTML = "";
-  document.querySelector(".alertaVTO").innerHTML = "";
-  document.querySelector(".alertaNombre").innerHTML = "";
-
-  if (nroTarjeta.length != 16) {
-    e.preventDefault();
-    document.querySelector(".alertaTarjeta").innerHTML =
-      "Número de tarjeta inválido";
-    hasError = true;
-  }
-  if (cod.length != 3 || cod == "000" || cod == "999") {
-    e.preventDefault();
-    document.querySelector(".alertaCod").innerHTML =
-      "Código de seguridad inválido";
-    hasError = true;
-  }
-  if (vto.length == 0) {
-    e.preventDefault();
-    document.querySelector(".alertaVTO").innerHTML =
-      "Fecha de vencimiento inválida";
-    hasError = true;
-  }
-  if (nombre.length == 0) {
-    e.preventDefault();
-    document.querySelector(".alertaNombre").innerHTML =
-      "Nombre y apellido inválidos";
-    hasError = true;
-  }
-
-  if (!hasError) {
-    e.preventDefault();
-    DIALOG.showModal();
-  }
-});
